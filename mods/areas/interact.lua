eeab43e779792632687c638fb311f9e59088327e
unknown <a-uro00630@tpead.gov.tw>
2016-11-29 09:40:33 +0800

    replace from Gundul's code about jail

--- mods/areas/interact.lua
+++ mods/areas/interact.lua
@@ -1,4 +1,4 @@
-
+--[[
 local old_is_protected = minetest.is_protected
 function minetest.is_protected(pos, name)
 	if not areas:canInteract(pos, name) then
@@ -16,4 +16,32 @@ minetest.register_on_protection_violation(function(pos, name)
 				table.concat(owners, ", ")))
 	end
 end)
+--]]
 
+-- from juli&Gundul
+local old_is_protected = minetest.is_protected
+function minetest.is_protected(pos, name)
+	 if not areas:canInteract(pos, name) then
+			return true
+	 end
+	 return old_is_protected(pos, name)
+end
+
+minetest.register_on_protection_violation(function(pos, name)
+
+	 if not areas:canInteract(pos, name) then
+			local owners = areas:getNodeOwners(pos)
+			minetest.chat_send_player(name,
+				 ("%s is protected by %s."):format(
+						minetest.pos_to_string(pos),
+						table.concat(owners, ", ")))
+
+			-- Begin here teleporting player to x,y,z if touching
+			 local player = minetest.get_player_by_name(name) -- local var for playername
+
+				if not player then return end
+
+				 player:setpos({x=638,y=5,z=473}) --- teleport to Coordinate x,y,z
+
+	 end
+end)
