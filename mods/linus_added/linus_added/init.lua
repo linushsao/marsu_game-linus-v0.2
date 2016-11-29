cc27963ea0a1981f57838e6df12ff16b0f2cac65
LinusHsao <linushsao@gmail.com>
2016-11-29 07:44:51 +0000

    update from juli&Gundul

--- mods/linus_added/linus_added/init.lua
+++ mods/linus_added/linus_added/init.lua
@@ -70,11 +70,42 @@ minetest.register_on_joinplayer(function(player)
 
 end)
 
+--from juli
+--[[
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
     if minetest.is_protected(pos, puncher) then
             print("jail...")
        puncher:setpos({x=-408, y=-4180, z=-97})
     end
+end)
+--]]
+
+--from Gundul
+local old_is_protected = minetest.is_protected
+function minetest.is_protected(pos, name)
+   if not areas:canInteract(pos, name) then
+      return true
+   end
+   return old_is_protected(pos, name)
+end
+
+minetest.register_on_protection_violation(function(pos, name)
+
+   if not areas:canInteract(pos, name) then
+      local owners = areas:getNodeOwners(pos)
+      minetest.chat_send_player(name,
+         ("%s is protected by %s."):format(
+            minetest.pos_to_string(pos),
+            table.concat(owners, ", ")))
+
+      -- Begin here teleporting player to x,y,z if touching
+       local player = minetest.get_player_by_name(name) -- local var for playername
+
+        if not player then return end
+
+         player:setpos({x=638,y=5,z=473}) --- teleport to Coordinate x,y,z
+
+   end
 end)
 
 
