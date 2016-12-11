d988623e25d822a72f1504663c49bed0dd4b17f1
LinusHsao <linushsao@gmail.com>
2016-12-11 13:14:21 +0000

    tweak for apprence of grieger log

--- server_side/moderators/moderators/record_griefer.lua
+++ server_side/moderators/moderators/record_griefer.lua
@@ -9,6 +9,7 @@ function minetest.is_protected(pos, name)
 end
 --]]
 
+--record the puncher,but puncher maybe just punch not get node.
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 
 
@@ -41,10 +42,10 @@ minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 		for _, v in pairs(owners) do
 				if v == puncher_name then check_area = true end
 		end
-		print(dump(check_area))
+--		print(dump(check_area))
 
 		if not check_area then font_color = "red" end
-		print(font_color)
+--		print(font_color)
 
 		grifer_pos[puncher_name] = pos
 
@@ -62,4 +63,59 @@ minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 		end
 
 	end
+end)
+
+
+--record the digger,digger means really get node.
+minetest.register_on_dignode(function(pos, oldnode, digger)
+
+		local old_is_protected = minetest.is_protected
+		local digger_name = digger:get_player_name()
+		local pos = pos
+		local check_area = false
+--[[
+		print("on_dignode outside")
+		print("########################################")
+		print("check_areas:canInteract "..dump(areas:canInteract(pos, digger)))
+		print("check pos:"..dump(pos))
+		print("check node:"..dump(oldnode))
+		print("check puncher:"..digger_name)
+--]]
+		if not areas:canInteract(pos, digger) then
+--			 print("on_dignode inside")
+			--record to log
+			--local puncher_name = puncher:get_player_name()
+			--local mypos = minetest.pos_to_string(pos) -- Sets variable to (X,Y,Z.. where Y is up)
+			local grifer_pos = {}
+			local griefer_file = minetest.get_worldpath() .. "/griefer_file"
+			local check_time = os.date("%x %H:%M")
+			local owners = areas:getNodeOwners(pos)
+
+			for _, v in pairs(owners) do
+					if v == digger_name then check_area = true end
+			end
+--			print(dump(check_area))
+
+			grifer_pos[digger_name] = pos
+
+			local changed = true
+
+			if changed then
+				local output = io.open(griefer_file, "a")
+					for i, v in pairs(grifer_pos) do
+						if check_area then
+							output:write("Time:"..check_time.."(CST),position:("..v.x..","..v.y..","..v.z.."),area_owner:"..table.concat(owners, ", ")..",digger:<b>"..i.."</b>,node:"..oldnode.name.."<br> \n ")
+						else
+						--<span style="color:#FFFFFF; background-color:#000000">
+							output:write("<span style="..'\"'.."color:#FFFFFF; background-color:#000000"..'\">'.."Time:"..check_time.."(CST),position:("..v.x..","..v.y..","..v.z.."),area_owner:"..table.concat(owners, ", ")..",digger:<font color=red><b>"..i.."</b></font>,node:"..oldnode.name.."</span><br> \n ")
+--							print("finished io.open")
+						end
+					end
+					io.close(output)
+					changed = false
+			end
+
+		end
+
+
 end)
