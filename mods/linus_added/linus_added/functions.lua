58602ac8a39cc0cca02e1857494fa68429c9fad0
LinusHsao <linushsao@gmail.com>
2017-01-02 21:04:05 +0000

    foo

--- mods/linus_added/linus_added/functions.lua
+++ mods/linus_added/linus_added/functions.lua
@@ -3,7 +3,7 @@
 function sky_change(player,ratio)
 
   if ratio > 0.83 or ratio < 0.18 then
-  print("STARRY NIGHT@@@@@@@@@@@@")
+--  print("STARRY NIGHT@@@@@@@@@@@@")
   player:set_sky({r=0, g=0, b=0},"skybox",{"marssurvive_space_sky.png","marssurvive_space_sky.png","marssurvive_space_sky_star.png","marssurvive_space_sky.png","marssurvive_space_sky.png","marssurvive_space_sky.png"})
   else
     if ratio < 0.5 then ratio = 1*(ratio/0.5)
@@ -11,7 +11,7 @@ function sky_change(player,ratio)
       if ratio > 0.8 then ratio = 1 end
       ratio = (1-ratio)/0.5
     end
-  	print("TIME RATOI in globalsetup:"..ratio)
+--  	print("TIME RATOI in globalsetup:"..ratio)
     player:set_sky({r=math.floor(219*ratio), g=math.floor(168*ratio), b=20+math.floor(117*ratio)},"plain",{})
   end
 end
