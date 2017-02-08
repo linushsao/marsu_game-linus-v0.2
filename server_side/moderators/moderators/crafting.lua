4e96358de689f7220a71d3800ee29f910b08c446
LinusHsao <linushsao@gmail.com>
2017-02-08 20:20:10 +0800

    foo

--- server_side/moderators/moderators/crafting.lua
+++ server_side/moderators/moderators/crafting.lua
@@ -12,22 +12,22 @@ minetest.register_tool("moderators:worldedit_tool", {
 				if player == playername then count = count + 1 end
 			end
 			print("count:"..count)
-	  if count > 2 then
+	  if count > 30 then
 	        minetest.chat_send_player(playername, "Worldedit only could be used twice per month")
       elseif is_md0(playername) then
   		    print("playername:"..playername)
   		    local privs = minetest.get_player_privs(playername)
   		    privs.worldedit = true
   		    minetest.set_player_privs(playername, privs)
-  		    
+
   		    --add playername in array
-			table.insert(worldedit_limits,1,playername) 
+			table.insert(worldedit_limits,1,playername)
 	        local worldedit_limits_file = minetest.get_worldpath() .. "/worldedit_limits"
 			print(dump(worldedit_limits))
 			local output = io.open(worldedit_limits_file, "w")
             output:write(minetest.serialize(worldedit_limits).."\n")
             io.close(output)
-			
+
             minetest.chat_send_player(playername, "Server have grant privs *worldedit* to "..playername)
   		    -- Takes one item from the stack
   		    itemstack:take_item()
