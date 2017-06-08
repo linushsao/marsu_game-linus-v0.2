5bc89a7e795b31249e2b8804a7f626828f6fd48f
cpdef <cpdef@protonmail.com>
2017-06-08 09:46:50 +0200

    add alias for spice turn to carrot

--- mods/linus_added/alias.lua
+++ mods/linus_added/alias.lua
@@ -5,6 +5,6 @@ minetest.register_alias("default:dirt_with_rainforest_litter", "default:dirt_wit
 minetest.register_alias("caverealms:constant_flame", "air")
 
 --for move to tenplus1's farming_redo
-minetest.register_alias("farming:spice_3", "farming:carrot_2")
+minetest.register_alias("farming:spice_3", "farming:cucumber_2")
 
 
