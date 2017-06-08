5d047f411d234237aa4683a59dac5b612f6081dd
cpdef <cpdef@protonmail.com>
2017-06-08 10:16:58 +0200

    old strawberry to new blueberry

--- mods/linus_added/alias.lua
+++ mods/linus_added/alias.lua
@@ -5,6 +5,12 @@ minetest.register_alias("default:dirt_with_rainforest_litter", "default:dirt_wit
 minetest.register_alias("caverealms:constant_flame", "air")
 
 --for move to tenplus1's farming_redo
-minetest.register_alias("farming:spice_3", "farming:cucumber_2")
+minetest.register_alias("farming:spice_1", "farming:cucumber_1")
+minetest.register_alias("farming:spice_3", "farming:cucumber_3")
+minetest.register_alias("farming:spice_2", "farming:cucumber_2")
+minetest.register_alias("farming:spice_4", "farming:cucumber_4")
+
+minetest.register_alias("farming:strawberry_3", "farming:blueberry_3")
+
 
 
