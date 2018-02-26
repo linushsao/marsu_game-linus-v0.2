ddaeb1e86141cab230005cf79313dcd6a4e5c96d
Tanmaya Meher <tanmayameher@users.noreply.github.com>
2018-02-26 15:39:27 +0530

    Update config.lua
    Reduced the time to generate air in air maker

--- mods/mars_pack/marsair/config.lua
+++ mods/mars_pack/marsair/config.lua
@@ -10,12 +10,12 @@ marsairconfig.need_air_generation_count = 8
 marsairconfig.need_air_fixleak_count = 4
 
 --the time one flower needs to generate an air-unit
-marsairconfig.flower_air_time = 20*60
+marsairconfig.flower_air_time = 2*60
 --the chance to generate air by flower after flower_air_time
-marsairconfig.flower_air_chance = 20
+marsairconfig.flower_air_chance = 30
 
 --same with tree
-marsairconfig.tree_air_time = 40*60
+marsairconfig.tree_air_time = 5*60
 marsairconfig.tree_air_chance = 30
 
 --the time until the tree-air becomes normal air and disappear
