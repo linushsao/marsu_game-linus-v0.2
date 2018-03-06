282149aaba1c6f2eafe4ca925cab324fb7503fd1
cpdef <cpdef@protonmail.com>
2018-03-06 10:03:04 +0100

    missing variable name

--- mods/linus_added/init.lua
+++ mods/linus_added/init.lua
@@ -70,7 +70,7 @@ minetest.register_globalstep(function(dtime)
 
 		for _,obj in ipairs(all_objects[v1]) do
 			if obj:is_player() then
-				if minetest.check_player_privs(name, {interact=false}) then
+				if minetest.check_player_privs(obj:get_player_name(), {interact=false}) then
 						
 					print("players found :"..obj:get_player_name())
 					if wiki_show[v1] == nil then wiki_show[v1]={} end
