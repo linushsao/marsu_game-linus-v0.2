6dd7941d9d6b96e84d5e753688067fd69aa70789
cpdef <cpdef@protonmail.com>
2018-03-22 15:03:30 +0100

    add new marsair crafts

--- mods/mars_pack/marsair/craft.lua
+++ mods/mars_pack/marsair/craft.lua
@@ -1,23 +1,52 @@
+minetest.register_craftitem("marsair:fan", {
+	description = "Mars Fan",
+	inventory_image = "marsair_fan.png",
+})
+
+minetest.register_craftitem("marsair:grid", {
+	description = "Mars airgenerator grid",
+	inventory_image = "marsair_grid.png",
+})
+
+minetest.register_craft({
+	output = "marsair:fan 2",
+	recipe = {
+		{"","default:steel_ingot",""},
+		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+		{"","default:steel_ingot",""},
+	}
+})
+
+minetest.register_craft({
+	output = "marsair:grid 6",
+	recipe = {
+		{"default:steel_ingot","default:steel_ingot",""},
+		{"", "", ""},
+		{"default:steel_ingot","default:steel_ingot",""},
+	}
+})
+
+
 minetest.register_craft({
 	output = "marsair:airgen",
 	recipe = {
 		{"marssurvive:shieldblock","marssurvive:shieldblock","marssurvive:shieldblock"},
-		{"marssurvive:shieldblock","marssurvive:oxogen", "marssurvive:shieldblock"},
+		{"marsair:grid", "marsair:fan", "marsair:grid"},
 	}
 })
 
 minetest.register_craft({
 	output = "marsair:airmaker",
 	recipe = {
-		{"marssurvive:shieldblock","marssurvive:shieldblock","marssurvive:shieldblock"},
-		{"marssurvive:shieldblock","marsair:airgen", "marssurvive:shieldblock"},
+		{"marssurvive:shieldblock","marsair:fan","marssurvive:shieldblock"},
+		{"marssurvive:shieldblock","default:dirt", "marssurvive:shieldblock"},
 	}
 })
 
 minetest.register_craft({
 	output = "marsair:tree_air_cleaner",
 	recipe = {
-		{"marssurvive:shieldblock","marssurvive:shieldblock","marssurvive:shieldblock"},
-		{"marssurvive:shieldblock","marsair:airmaker", "marssurvive:shieldblock"},
+		{"marssurvive:shieldblock","marsair:fan","marssurvive:shieldblock"},
+		{"marsair:fan","", "marsair:fan"},
 	}
 })
