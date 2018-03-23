e5c370186a8a3eedd2540076e00fc43abffe5e8f
cpdef <cpdef@protonmail.com>
2018-03-23 14:14:21 +0100

    remove double seeds

--- mods/administration/linus_added/alias.lua
+++ mods/administration/linus_added/alias.lua
@@ -21,6 +21,36 @@ minetest.register_alias("farming:seed_strawberry", "farming:blueberries")
 minetest.register_alias("farming:seed_tomato", "farming:tomato")
 minetest.register_alias("farming:seed_potato", "farming:potato")
 
+--remove double plants
+
+minetest.register_alias_force("farming_plus:rhubarb_seed", "farming:rhubarb")
+minetest.register_alias_force("farming_plus:rhubarb", "farming:rhubarb_3")
+minetest.register_alias_force("farming_plus:rhubarb_1", "farming:rhubarb_1")
+minetest.register_alias_force("farming_plus:rhubarb_2", "farming:rhubarb_2")
+minetest.register_alias_force("farming_plus:rhubarb_item", "farming:rhubarb")
+
+minetest.register_alias_force("farming_plus:carrot_seed", "farming:carrot")
+minetest.register_alias_force("farming_plus:carrot", "farming:carrot_4")
+minetest.register_alias_force("farming_plus:carrot_1", "farming:carrot_1")
+minetest.register_alias_force("farming_plus:carrot_2", "farming:carrot_2")
+minetest.register_alias_force("farming_plus:carrot_3", "farming:carrot_3")
+minetest.register_alias_force("farming_plus:carrot_item", "farming:carrot")
+
+minetest.register_alias_force("farming_plus:tomato_seed", "farming:tomato")
+minetest.register_alias_force("farming_plus:tomato", "farming:tomato_4")
+minetest.register_alias_force("farming_plus:tomato_1", "farming:tomato_1")
+minetest.register_alias_force("farming_plus:tomato_2", "farming:tomato_2")
+minetest.register_alias_force("farming_plus:tomato_3", "farming:tomato_3")
+minetest.register_alias_force("farming_plus:tomato_item", "farming:tomato")
+
+minetest.register_alias_force("farming_plus:potato_seed", "farming:potato")
+minetest.register_alias_force("farming_plus:potato", "farming:potato_3")
+minetest.register_alias_force("farming_plus:potato_1", "farming:potato_1")
+minetest.register_alias_force("farming_plus:potato_2", "farming:potato_2")
+minetest.register_alias_force("farming_plus:potato_item", "farming:potato")
+
+
+
 --remove pollution
 minetest.register_alias("pollution:dirt", "marssurvive:clayblock")
 minetest.register_alias("pollution:water_source", "default:water_source")
